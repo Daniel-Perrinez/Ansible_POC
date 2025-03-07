029efb1a80c37ba6fbaefadaea12b8a5a50720f5
Daniel-Perrinez <you@example.com>
2025-03-06 23:15:46 -0500

    added route53.tf

---
+++ AWS_Intro_to_Ansible/3-Advanced/TF/route53.tf
@@ -0,0 +1,15 @@
+# resource "aws_route53_zone" "dev" {
+#   name = "wei-ansible-dev.com"
+# }
+
+# resource "aws_route53_record" "www" {
+#   zone_id = aws_route53_zone.dev.zone_id
+#   name    = "www.wei-ansible-dev.com"
+#   type    = "A"
+#   ttl     = "300"
+#   records = [aws_instance.dev-linux-vm.public_ip]
+# }
+
+
+# DEV - 34.229.132.203 --> http://ec2-34-229-132-203.compute-1.amazonaws.com
+# PROD - 3.227.240.148 -->  http://ec2-3-227-240-148.compute-1.amazonaws.com
